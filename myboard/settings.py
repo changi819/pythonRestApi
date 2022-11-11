7484b2d0095b01a54bcca33c17b9eecc1a6f6147
Min Leo <mck0819@gmail.com>
2022-11-11 16:59:03 +0900

    key modify

--- myboard/settings.py
+++ myboard/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['secret_key']
-
+#django-insecure-qav)qnawf04thjnhfoymvux@ga^04qo4heju10cw59@7t(wt#5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
